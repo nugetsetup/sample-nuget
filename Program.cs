bd9fea6182322f7e5c9fa8cc7600f5c48545ff75
namratajha <namratajha@github.com>
2022-09-05 13:51:07 +0530

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sample_nuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
